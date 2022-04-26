e96e4592cedb8adad322753ccb34b51e43747624
Abrar Zaman <abrarzaman2003@gmail.com>
2022-04-26 01:29:47 -0500

    python

--- bitmap2.asm
+++ bitmap2.asm
@@ -68,8 +68,8 @@ gameLoop:
 	
 	subi $s2, $s2, 48
 	
-	blt $s2, $zero, gameLoop
-	bgt $s2, $s7, gameLoop
+	blt $s2, $zero, invalidMove #invalid character Checking
+	bgt $s2, $s7, invalidMove #invalid character Checking
 	
 	move	$a0, $s2
 	
@@ -79,7 +79,7 @@ gameLoop:
 	
 	lb $s5, heightMap($s2)
 	
-	beq $s5, $s6, gameLoop #if theres too many chips, then it just goes back to game loop
+	beq $s5, $s6, invalidMove #if theres too many chips, then it just goes back to game loop
 	
 	move $a1, $s5
 	
@@ -467,7 +467,143 @@ drawCircleRow: #$a0-> base address $a1-> row num $a2->row size
 	jr $ra
 	
 	
-	
- 
-   
+
+invalidMove:
+	li	$v0, 3
+	jal	choose_sound
+	j gameLoop
+		
+choose_sound:
+beq $v0, 0, sound_place
+beq $v0, 1, sound_win
+beq $v0, 2, sound_lose
+beq $v0, 3, sound_invalid
+beq $v0, 4, sound_computer_place
+
+
+sound_place:
+li $a0, 60
+li $a1, 300
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 67
+li $a1, 300
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+j end
+
+sound_win:
+li $a0, 55
+li $a1, 200
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 60
+li $a1, 200
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 64
+li $a1, 200
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 67
+li $a1, 400
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 64
+li $a1, 200
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 67
+li $a1, 400
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+j end
+
+sound_lose:
+li $a0, 67
+li $a1, 100
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 63
+li $a1, 100
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 60
+li $a1, 100
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 48
+li $a1, 100
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 44
+li $a1, 100
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 41
+li $a1, 100
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+j end
+
+sound_invalid:
+li $a0, 45
+li $a1, 200
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 45
+li $a1, 200
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+j end
+
+sound_computer_place:
+li $a0, 67
+li $a1, 300
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+li $a0, 60
+li $a1, 300
+li $a2, 104
+li $a3, 127
+li $v0, 33
+syscall
+j end
+
+end:
+  jr $ra
     
---
+++ pixelEncoder.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot
+img = cv2.imread('./pixil-frame-0.png')
+f= open("bruh2.txt","w+")
+x = 90
+y = 92
+print ('RGB shape: ', img.shape)
+print (img[x, y])
+# print((img[x, y][0] and img[x,y][1] and img[x,y][2]) == 0)
+
+
+
+
+for i in range(0,256):
+	for j in range(0,256):
+		q = img[j,i]
+		if ((q[0] and q[1] and q[2]) != 0):
+			# print("{} comma {}".format(i, j))
+			# print(img[j,i])
+			offset = (i*4)+(j*1024)
+			string = "sw $s2, {}($s0)\n".format(offset)
+			f.write(string)
+
+f.close()
+			
+
+
+# pyplot.imshow(img)
+# pyplot.show()
+
