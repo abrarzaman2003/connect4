f424b87f017ef62032997e65ad946d772cd62437
Abrar Zaman <abrarzaman2003@gmail.com>
2022-04-27 15:30:09 -0500

    further progress with the logic

--- bitmap2.asm
+++ bitmap2.asm
@@ -1,21 +1,31 @@
 #bitmap settings: 2by2 unit size, 512 by 512 display size
 
 .data
+  newLine: .asciiz "\n"
   displayAddress:	.word	0x10008000 # base address of the display
   heightMap: 		.word 	0x05050505, 0x00050505
   board:		.space	48
   yellowColor: .word 0xfed700
   redColor: .word 0x00ffffff
   
-  
-.macro max(%a %b) #outputs to $v0
-    bgt %a %b b1
-    add %a %b $zero
-b1:
-.end_macro
+  .macro convertBoardCoords %x %y %r #takes in the x,y values and converts into the offset to store in r
+ 	sll %r %y 3
+ 	add %r %r %x
+ .end_macro 
+ 
+ .macro max %x %y %r #sets r to the max of x and y
+ 	blt  %x, %y, g
+ 	move %r %x
+ 	j s
+ 	g:
+ 	move %r %y
+ 	s:
+ .end_macro
+
+
 .macro save ()
-addiu   $sp, $sp, -76 #allocates stack memory for the return address
-        sw $ra, ($sp)
+addiu   $sp, $sp, -92 #allocates stack memory for the return address
+        sw $ra ($sp)
         sw $t0 4($sp)
         sw $t1 8($sp)
         sw $t2 12($sp)
@@ -34,6 +44,10 @@ addiu   $sp, $sp, -76 #allocates stack memory for the return address
         sw $s5 64($sp)
         sw $s6 68($sp)
         sw $s7 72($sp)
+        sw $a0 76($sp)
+        sw $a1 80($sp)
+        sw $a2 84($sp)
+        sw $a3 88($sp)
 .end_macro
 
 .macro restor()
@@ -56,345 +70,11 @@ addiu   $sp, $sp, -76 #allocates stack memory for the return address
         lw $s5 64($sp)
         lw $s6 68($sp)
         lw $s7 72($sp)
-        addiu    $sp, $sp, 76 #reallocates the stack 
-.end_macro
-
- .macro set_board (%x %v)#takes the height as an input and checks the heighmap to figure out where to make the circle t0,
-    lbu $t0 heightMap(%x) #obtains the height of the column that we need to place the piece on
-    
-    #moves these parameters to the arguments of the make circle function
-    move    $a0, %x 
-    move    $a1, $t0
-    
-    #decrements the height and stores it in the correct position in the height map, and reincrements it to continue with the calculations
-    addi $t0, $t0, -1
-    sb    $t0, heightMap(%x)
-    addi $t0, $t0, 1
-    
-    
-    #shifts the height by 3 to multiply it by 8, since there are 8 bytes in each row of the table, this would be like going down (the height) number of rows
-    sll $t0, $t0, 3
-    add $t0, $t0, %x #adds x to it to get to the correct position
-    sb  %v, board($t0) #stores the correct byte into the position of the board
-    
-    li $t0, 1 #loads 1 into $t0 for logic with picking the color
-    
-    beq %v, $t0, yellow #if v == 0, then it places a yellow chip
-    
-    #else a red chip is placed 
-    lw $t0, redColor
-    
-    move    $a3, $t0
-    j over #placed to jump over the yellow code
-    
-    
-    yellow:
-    lw $t0, yellowColor
-    move    $a3, $t0
-    
-    over:
-    jal makeCircle #calls the make circle function
-    
-.end_macro
-
-.macro    get_board (%x, %y) #outputs to $t5
-    sll    $t0, %y, 3 #shifting to get uh the uh correct row
-    add    $t0, $t0, %x #adding to get the correct byte 
-    lbu    $t5 board($t0) #loading that byte into the t5 register
-.end_macro
-  
-.macro gcq (%x, %y, %v) #uses $t1-9 and outputs to $v0
-#horizontal
-	li $v0 0 #g
-	li $t1 0 #c
-	li $t2 1 #l
-	li $t3 1 #r
- 	li $t7 1 #i
-for1:	sub $t5 %x $t2 #x-l
-	blt $t5 $zero b1_2 #x-l<0
-	get_board ($t5 %y) #(x-l,y)
-	bne $t5 %v b1_2  #v1==b[x-l][y]
-	addi $t1 $t1 1 #c++
-	addi $t2 $t2 1 #l++
-	j inc1
-b1_2:	add $t5 %x $t3 #x+r
-	li $t6 6
-	bgt $t5 $t6 b1_3 #x+r>6
-	get_board ($t5 %y) #(x+r,5)
-	bne $t5 %v b1_3	#v1=b[x+r][y]
-	addi $t1 $t1 1	#c++
-	addi $t3 $t3 1	#r++
-	j inc1
-b1_3:	sub $t5 %x $t2	#x-l
-	blt $t5 $zero b1_4 #x-l<0
-	get_board ($t5 %y) #v=b[x-l][y]
-	bne $t5 $zero b1_4 #v1==0
-	addi $t2 $t2 1 #l++
-	j inc1
-b1_4:	add $t5 %x $t3 #x+r
-	li $t6 6
-	bgt $t5 $t6 inc1 #x+r>6
-	get_board ($t5 %y) #v1=b[x+r][y]
-	bne $t5 $zero gend1 #v==0
-	addi $t3 $t3 1 #r++
-	j inc1
-inc1:	addi $t7 $t7 1 #i++
-	li $t8 4
-	bne $t7 $t8 for1 #i!=4
-gend1:	max ($v0 $t1) #v=max(v c)
-	
-#vertical
-	li $t1 0 #c
-	li $t2 1 #l
-	li $t3 1 #r
- 	li $t7 1
-for2:	sub $t5 %y $t2
-	blt $t5 $zero b2_2
-	get_board (%x $t5)
-	bne $t5 %v b2_2	
-	addi $t1 $t1 1
-	addi $t2 $t2 1
-	j inc2
-b2_2:	add $t5 %y $t3
-	li $t6 5
-	bgt $t5 $t6 b2_3
-	get_board (%x $t5)
-	bne $t5 %v b2_3	
-	addi $t1 $t1 1
-	addi $t3 $t3 1
-	j inc2
-b2_3:	sub $t5 %y $t2
-	blt $t5 $zero b2_4
-	get_board (%x $t5)
-	bne $t5 $zero b2_4	
-	addi $t2 $t2 1
-	j inc2
-b2_4:	add $t5 %y $t3
-	li $t6 5
-	bgt $t5 $t6 inc2
-	get_board (%x $t5)
-	bne $t5 $zero gend2	
-	addi $t3 $t3 1
-	j inc2
-inc2:	addi $t7 $t7 1
-	li $t8 4
-	bne $t7 $t8 for2
-gend2:	max($v0 $t1)
-
-#diagonal like '/'
-	li $t1 0 #c
-	li $t2 1 #l
-	li $t3 1 #r
- 	li $t7 1
-for3:	sub $t5 %y $t2
-	sub $t9 %x $t2
-	blt $t5 $zero b3_2
-	blt $t9 $zero b3_2
-	get_board ($t9 $t5)
-	bne $t5 %v b3_2	
-	addi $t1 $t1 1
-	addi $t2 $t2 1
-	j inc3
-b3_2:	add $t5 %y $t3
-	add $t9 %x $t3
-	li $t6 5
-	bgt $t5 $t6 b3_3
-	li $t6 6
-	bgt $t9 $t6 b3_3
-	get_board ($t9 $t5)
-	bne $t5 %v b3_3	
-	addi $t1 $t1 1
-	addi $t3 $t3 1
-	j inc3
-b3_3:	sub $t5 %y $t2
-	sub $t9 %x $t2
-	blt $t5 $zero b3_4
-	blt $t9 $zero b3_4
-	get_board ($t9 $t5)
-	bne $t5 $zero b3_4	
-	addi $t2 $t2 1
-	j inc3
-b3_4:	add $t5 %y $t3
-	add $t9 %x $t3
-	li $t6 5
-	bgt $t5 $t6 gend3
-	li $t6 6
-	bgt $t9 $t6 gend3
-	get_board ($t9 $t5)
-	bne $t5 $zero gend3
-	addi $t3 $t3 1
-inc3:	addi $t7 $t7 1
-	li $t8 4
-	bne $t7 $t8 for3
-gend3:	max($v0 $t1)
-
-#diagonals that look like '\'
-	li $t1 0 #c
-	li $t2 1 #l
-	li $t3 1 #r
- 	li $t7 1
-for4:	sub $t5 %y $t2
-	add $t9 %x $t2
-	blt $t5 $zero b4_2
-	li $t6 6
-	bgt $t9 $t6 b4_2
-	get_board ($t9 $t5)
-	bne $t5 %v b4_2	
-	addi $t1 $t1 1
-	addi $t2 $t2 1
-	j inc4
-b4_2:	add $t5 %y $t3
-	sub $t9 %x $t3
-	blt $t9 $zero b4_3
-	li $t6 5
-	bgt $t9 $t6 b4_3
-	get_board ($t9 $t5)
-	bne $t5 %v b4_3	
-	addi $t1 $t1 1
-	addi $t3 $t3 1
-	j inc4
-b4_3:	li $t7 1
-	sub $t5 %y $t2
-	add $t9 %x $t2
-	blt $t5 $zero b4_4
-	li $t6 6
-	bgt $t9 $t6 b4_4
-	get_board ($t9 $t5)
-	bne $t5 $zero b4_4	
-	addi $t2 $t2 1
-	j inc4
-b4_4:	add $t5 %y $t3
-	sub $t9 %x $t3
-	blt $t9 $zero gend4
-	li $t6 5
-	bgt $t9 $t6 gend4
-	get_board ($t9 $t5)
-	bne $t5 $zero gend4
-	addi $t3 $t3 1
-inc4:	addi $t7 $t7 1
-	li $t8 4
-	bne $t7 $t8 for4
-gend4:	max($v0 $t1)
-.end_macro
-
-.macro win () #uses $s0-4, $t0 and outputs to $v1 (1 win, 0 lose)
-	li $v1 0
-	li $s0 0
-	li $s1 7 
-	
-forw1:	li $s2 0
-	li $s3 6
-	li $s4 1
-forwi:	gcq($s0 $s2 $s4)
-	li $t0 3
-	bne $v0 $t0 nw
-	get_board ($s0 $s2)
-	li $t0 1
-	bne $v0 $t0 nw
-	li $v1 1
-nw:	li $s4 2
-	gcq($s0 $s2 $s4)
-	li $t0 3
-	bne $v0 $t0 incw
-	get_board ($s0 $s2)
-	li $t0 1
-	bne $v0 $t0 incw
-	li $v1 1
-	
-incw:	
-	addi $s2 $s2 1
-	bne $s2 $s3 forwi
-	addi $s0 $s0 1
-	bne $s0 $s1 forw1
-.end_macro
-
-.macro prio (%x) #uses $s0-4, $t0 and outputs to $v1, reorder so small is better
-	lbu $s0 heightMap(%x) #y
-	blt $s0 $zero pend #y<0
-	li $t0 3
-	sub $s1 $t0 %x
-	sra $t0,$s1,31   
-	xor $s1,$s1,$t0
-	sub $s1,$s1,$t0
-	li $t0 3 
-	sub $s1 $t0 $s1 #o
-	li $t0 1 
-	gcq(%x $s0 $t0)
-	add $s3 $v0 $zero #g1
-	li $t0 2
-	gcq(%x $s0 $t0)
-	add $s4 $v0 $zero #g2
-	li $t0 3
-	bne $s3 $t0 pb1 #g1==3
-	addi $v1 $s1 80
-	j pend
-pb1:	bne $s4 $t0 pb2 #g2==3
-	addi $v1 $s1 70
-	j pend
-pb2:	li $t0 2
-	bne $s3 $t0 pb3
-		bne $s4 $t0 pb3
-		addi $v1 $s1 60
-		j pend
-pb3:	bne $s4 $t0 pb4
-		addi $v1 $s1 50
-		j pend
-pb4:	bne $s3 $t0 pb5
-		addi $v1 $s1 40
-		j pend
-pb5:	li $t0 1
-		bne $s3 $t0 pb6
-		addi $v1 $s1 30
-		j pend
-pb6:	bne $s4 $t0 pb7
-		addi $v1 $s1 20
-		j pend
-pb7:	addi $v1 $s1 10
-pend:
-.end_macro
-
-.macro opps() #uses $s5-7
-	save()
-	li $s5 -1 #x
-	li $s6 0 #p (score)
-	li $s7 0 #i
-	prio ($s7)
-	blt $v1 $s6 on1
-	add $s6 $v1 $zero
-	add $s5 $s7 $zero
-on1:	li $s7 1 #i
-	prio ($s7)
-	blt $v1 $s6 on2
-	add $s6 $v1 $zero
-	add $s5 $s7 $zero
-on2:	li $s7 2 #i
-	prio ($s7)
-	blt $v1 $s6 on3
-	add $s6 $v1 $zero
-	add $s5 $s7 $zero
-on3:	li $s7 3 #i
-	prio ($s7)
-	blt $v1 $s6 on4
-	add $s6 $v1 $zero
-	add $s5 $s7 $zero
-on4:	li $s7 4 #i
-	prio ($s7)
-	blt $v1 $s6 on5
-	add $s6 $v1 $zero
-	add $s5 $s7 $zero
-on5:	li $s7 5 #i
-	prio ($s7)
-	blt $v1 $s6 on6
-	add $s6 $v1 $zero
-	add $s5 $s7 $zero
-on6:	li $s7 6 #i
-	prio ($s7)
-	blt $v1 $s6 oend
-	add $s6 $v1 $zero
-	add $s5 $s7 $zero
-	li $s6 2
-oend:	set_board ($s5 $s6)
-	restor()
+        lw $a0 76($sp)
+        lw $a1 80($sp)
+        lw $a2 84($sp)
+        lw $a3 88($sp)
+        addiu    $sp, $sp, 92 #reallocates the stack 
 .end_macro
   
 .text
@@ -461,10 +141,17 @@ gameLoop:
 	blt $s2, $zero, invalidMove #invalid character Checking
 	bgt $s2, $s7, invalidMove #invalid character Checking
 	
-	li $t1, 1
-	set_board $s2, $t1
+	save()
+	move $a0, $s2
+	li   $a1, 1
+	jal set_board
+	restor()
 	
-	opps()
+	save()
+	jal printBoard
+	restor()
+	
+	jal opps
 	
 	#move	$a0, $s2
 	
@@ -992,4 +679,376 @@ j end
 
 end:
   jr $ra
+  
+  
+gcq: #args: a0 == x, a1==y (bottom row 5, top 0) a2==v, output to v0 {
+	save()
+	li $s0 0 #g
+	
+	
+	li $s6, 6 #holds constant 6
+	li $s5, 5 #holds constant 5
+	
+	li $s1 0 #c
+	li $s2 1 #l
+	li $s3 1 #r
+	
+	li $t0, 1 #int i = 1
+	li $t1, 4 # holds 4 for i<4
+	
+	horizontalForLoop:
+		
+		sub $t2, $a0, $s2 #x-l ==> t2
+		blt $t2, $zero, helse1 #x-l < 0	
+			convertBoardCoords $t2, $a1, $t3 
+			lbu $t4, board($t3) #places the value of board[x-l][y] into t4 for comparison
+			bne $t4, $a2, helse1 #board[x-l][y] == v #using bne becuase if its not equal, then it goes to else, but if it is equal, it just starts executing code below the line
+				addi $s1 $s1, 1 #c++
+				addi $s2 $s2, 1 #l++
+				j hdone
+		helse1:
+		add $t5, $a0, $s3 #x+r ==> t5
+		bgt $t5, $s6, helse2 #x+r > 6
+			convertBoardCoords $t5, $a1, $t6 #places value of board[x+r][y] into t6 for comparison
+			lbu $t7, board($t6) #places the value of board[x-l][y] into t7 for comparison
+			bne $t7, $a2, helse2 #board[x+r][y] == v
+				addi $s1 $s1, 1 #c++
+				addi $s3 $s3, 1 #r++
+				j hdone
+		helse2:
+		blt $t2, $zero, helse3 #x-1 < 0
+			bne $t4, $zero, helse3 #board[x-l][y] == 0
+				addi $s2 $s2, 1 #l++
+				j hdone
+		helse3:
+		bgt $t5, $s6, helse4 #x+r > 6
+			bne $t7, $zero, helse4 #board[x+r][y] == 0
+				addi $s3 $s3, 1 #r++
+				j hdone
+		helse4:
+			j break1 #breaks out the loop
+		
+		hdone:	#outside of the if
+		addi $t0, $t0, 1 #i++
+		blt $t0, $t1, horizontalForLoop #i<4
+		# end of loop
+		
+		break1:
+		
+		max $s0, $s1, $s0 #g = max (g,c)
+
+
+		li $s1 0 #c
+		li $s2 1 #l
+		li $s3 1 #r
+	
+		li $t0, 1 #int i = 1
+		li $t1, 4 # holds 4 for i<4
+	
+	verticalForLoop: #{
+		
+		sub $t2, $a1, $s2 #y-l ==> t2
+		blt $t2, $zero, velse1 #y-l < 0	
+			convertBoardCoords $a0, $t2, $t3 
+			lbu $t4, board($t3) #places the value of board[x-l][y] into t4 for comparison
+			bne $t4, $a2, velse1 #board[x][y-l] == v #using bne becuase if its not equal, then it goes to else, but if it is equal, it just starts executing code below the line
+				addi $s1 $s1, 1 #c++
+				addi $s2 $s2, 1 #l++
+				j vdone
+		velse1:
+		add $t5, $a1, $s3 #y+r ==> t5
+		bgt $t5, $s5, velse2 #y+r > 5
+			convertBoardCoords $a0, $t5, $t6 #places value of board[x+r][y] into t6 for comparison
+			lbu $t7, board($t6) #places the value of board[x][y+r] into t7 for comparison
+			bne $t7, $a2, velse2 #board[x][y+r] == v
+				addi $s1 $s1, 1 #c++
+				addi $s3 $s3, 1 #r++
+				j vdone
+		velse2:
+		blt $t2, $zero, velse3 #x-1 < 0
+			bne $t4, $zero, velse3 #board[x][y-l] == 0
+				addi $s2 $s2, 1 #l++
+				j vdone
+		velse3:
+		bgt $t5, $s5, velse4 #y+r > 6
+			bne $t7, $zero, velse4 #board[x][y+r] == 0
+				addi $s3 $s3, 1 #r++
+				j vdone
+		velse4:
+			j break2 #breaks out the loop
+		
+		vdone:	#outside of the if
+		addi $t0, $t0, 1 #i++
+		blt $t0, $t1, verticalForLoop #i<4
+		# end of loop
+		
+		break2:
+		
+		max $s0, $s1, $s0 #g = max (g,c) #}
+
+		li $s1 0 #c
+		li $s2 1 #l
+		li $s3 1 #r
+	
+		li $t0, 1 #int i = 1
+		li $t1, 4 # holds 4 for i<4
+
+
+	diagonalForLoop1: #like \ {
+
+		sub $t2, $a0, $s2 #x-l ==> t2
+		sub $t8, $a1, $s2 #y-l ==> t8
+		blt $t2, $zero, d1else1 #x-l < 0	
+		blt $t8, $zero, d1else1 #y-l <0
+			convertBoardCoords $t2, $t8, $t3 
+			lbu $t4, board($t3) #places the value of board[x-l][y-l] into t4 for comparison
+			bne $t4, $a2, d1else1 #board[x-l][y-l] == v #using bne becuase if its not equal, then it goes to else, but if it is equal, it just starts executing code below the line
+				addi $s1 $s1, 1 #c++
+				addi $s2 $s2, 1 #l++
+				j d1done
+		d1else1:
+		add $t5, $a0, $s3 #x+r ==> t5
+		add $t9, $a1, $s3 #y+r ==> t9
+		bgt $t5, $s6, d1else2 #x+r > 6
+		bgt $t9, $s5, d1else2 #y+r > 5
+			convertBoardCoords $t5, $t9, $t6 #places value of board[x+r][y] into t6 for comparison
+			lbu $t7, board($t6) #places the value of board[x-l][y] into t7 for comparison
+			bne $t7, $a2, d1else2 #board[x+r][y+r] == v
+				addi $s1 $s1, 1 #c++
+				addi $s3 $s3, 1 #r++
+				j d1done
+		d1else2:
+		bgt $t5, $s6, d1else4 #x+r > 6
+		bgt $t9, $s5, d1else4 #y+r > 5
+			bne $t7, $zero, d1else4 #board[x+r][y+r] == 0
+				addi $s3 $s3, 1 #r++
+				j d1done
+		d1else3:
+		blt $t2, $zero, d1else3 #x-1 < 0
+		blt $t8, $zero, d1else3 #y-l <0
+			bne $t4, $zero, d1else3 #board[x-l][y-l] == 0
+				addi $s2 $s2, 1 #l++
+				j d1done
+		d1else4:
+			j break3 #breaks out the loop
+		
+		d1done:	#outside of the if
+		addi $t0, $t0, 1 #i++
+		blt $t0, $t1, diagonalForLoop1 #i<4
+		# end of loop
+		
+		break3:
+		
+		max $s0, $s1, $s0 #g = max (g,c) #}
+
+
+		li $s1 0 #c
+		li $s2 1 #l
+		li $s3 1 #r
+	
+		li $t0, 1 #int i = 1
+		li $t1, 4 # holds 4 for i<4
+
+	diagonalForLoop2: #like /
+
+		sub $t2, $a0, $s2 #x-l ==> t2
+		add $t9, $a1, $s2 #y+l ==> t9
+		blt $t2, $zero, d2else1 #x-l < 0	
+		bgt $t9, $s5, d2else1 #y+l > 5
+			convertBoardCoords $t2, $t9, $t3 
+			lbu $t4, board($t3) #places the value of board[x-l][y+l] into t4 for comparison
+			bne $t4, $a2, d2else1 #board[x-l][y+l] == v #using bne becuase if its not equal, then it goes to else, but if it is equal, it just starts executing code below the line
+				addi $s1 $s1, 1 #c++
+				addi $s2 $s2, 1 #l++
+				j d2done
+		d2else1:
+		add $t5, $a0, $s3 #x+r ==> t5
+		sub $t8, $a1, $s3 #y-r ==> t8
+		bgt $t5, $s6, d2else2 #x+r > 6
+		blt $t8, $zero d2else2 #y-r < 0
+			convertBoardCoords $t5, $t8, $t6 #places value of board[x+r][y-r] into t6 for comparison
+			lbu $t7, board($t6) #places the value of board[x+r][y-r] into t7 for comparison
+			bne $t7, $a2, d2else2 #board[x+r][y+r] == v
+				addi $s1 $s1, 1 #c++
+				addi $s3 $s3, 1 #r++
+				j d2done
+		d2else2:
+		blt $t2, $zero, d2else3 #x-l < 0	
+		bgt $t9, $s5, d2else3 #y+l > 5
+			bne $t4, $zero, d2else4 #board[x-l][y+l] == 0
+				addi $s2 $s2, 1 #l++
+				j d2done
+		d2else3:
+		bgt $t5, $s6, d2else4 #x+r > 6
+		blt $t8, $zero d2else4 #y-r < 0
+			bne $t7, $zero, d2else3 #board[x+r][y-r] == 0
+				addi $s3 $s3, 1 #r++
+				j d2done
+		d2else4:
+			j break4 #breaks out the loop
+		
+		d2done:	#outside of the if
+		addi $t0, $t0, 1 #i++
+		blt $t0, $t1, diagonalForLoop2 #i<4
+		# end of loop
+		
+		break4:
+		
+		max $s0, $s1, $s0 #g = max (g,c)
+		move $v0 $s0
+		restor()
+		jr $ra #} 
+
+prio: # $a0 == x, output to $v0 
+	save()
+	lbu $a1 heightMap($a0) #s0 = y
+	blt $a1 $zero pend #y<0
+	li $t0 3
+	sub $s1 $t0 $a0
+	sra $t0,$s1,31   
+	xor $s1,$s1,$t0
+	sub $s1,$s1,$t0 #s1 = abs(3-x)
+	li $t0 3 
+	sub $s1 $t0 $s1 #s1 = o = 3-abs(3-x)
+	li $a2 1 
+	jal gcq
+	add $s3 $v0 $zero #$s3 = g1
+	li $a2 2
+	jal gcq
+	add $s4 $v0 $zero #$s4 = g2
+	li $t0 3
+		bne $s3 $t0 pb1 #g1==3
+		addi $v0 $s1 80
+		j pend
+	pb1:	bne $s4 $t0 pb2 #g2==3
+		addi $v0 $s1 70
+		j pend
+	pb2:	li $t0 2
+		bne $s3 $t0 pb3 #g1==2
+		bne $s4 $t0 pb3 #g2==2
+		addi $v0 $s1 60
+		j pend
+	pb3:	bne $s4 $t0 pb4 #g1==2
+		addi $v0 $s1 50
+		j pend
+	pb4:	bne $s3 $t0 pb5 #g2==2
+		addi $v0 $s1 40
+		j pend
+	pb5:	li $t0 1
+		bne $s3 $t0 pb6 #g1==1
+		addi $v0 $s1 30
+		j pend
+	pb6:	bne $s4 $t0 pb7 #g2==1
+		addi $v0 $s1 20
+		j pend
+	pb7:	addi $v0 $s1 10
+	pend: restor()
+		jr $ra
+
+printBoard:
+#implement registers used: $t0, $t1, $t2, $s0, $s1 {
+    li $v0, 1
+    li $t0, 0 #for each row, there needs to be 8 bytes outputted
+    li $s0, 7
+
+    li $t1, 0 #there should be 6 columns outputted as well
+    li $s1, 6
+    ploop:
+
+
+    sll $t2, $t1, 3
+    add $t2, $t2, $t0
+
+    lbu $a0, board($t2)
+    syscall
+
+    addi $t0, $t0, 1
+
+    bne $t0, $s0, ploop
+
+    li $t0 0
+    addi $t1, $t1, 1
+
+    la $a0, newLine
+    li $v0, 4
+    syscall
+
+    li $v0 1
+
+    bne $t1, $s1, ploop
+
+    la $a0, newLine
+    li $v0, 4
+    syscall
+
+    jr $ra#}
+
+set_board:
+#implement- the input should be the column number ($a0) and either a 1 or 2 depending on the color of the chip ($a1), no return
+#registers used: $t0, $ra {
+
+    lbu $t0, heightMap($a0)
     
+    addi $t0, $t0, -1
+    sb $t0, heightMap($a0)
+    addi $t0, $t0, 1
+    
+   li $s0, 1
+    
+    
+    save()
+    beq $a1, $s0, yellow
+    li 	$a3, 0xff0000
+    j over1
+    
+    yellow:
+    li 	$a3, 0xfed700
+    
+    over1:
+    #a0 is already set
+    move $a1, $t0
+    jal makeCircle
+    restor()
+    
+    sll $t0, $t0, 3
+    add $t0, $t0, $a0
+    
+    sb $a1, board($t0)
+    
+    jr $ra #}
+ 
+ 
+opps: #no input, no return
+save
+	li $s0, -1
+	li $s1, 100
+	li $s2, 7
+	
+	li $t0, 0
+	
+	forOp:
+		move $a0, $t0
+		jal prio
+			 #a would be in v(0)
+		bge $v0, $s1, elseO # if (a<p) #used bge so that if its greater than or equal it would go to else, if not it will execute the shit below
+			move $s1, $v0
+			move $s0, $t0
+		elseO:		
+	addi $t0, $t0, 1
+	blt $t0, $s2, forOp
+		#end of loop
+	
+	addiu $sp, $sp, -12
+	sw $ra ($sp)
+	sw $t0 4($sp)
+	
+	move $a0, $s0
+	li $a1, 2
+	jal set_board
+	
+	lw $ra ($sp)
+	lw $t0 4($sp)
+	addiu $sp, $sp, 12
+restor
+	jr $ra
+	
